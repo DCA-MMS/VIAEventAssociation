39b9d9e58e24edc2a1fab8c521e8c4ae5ad4d43d
ForgottenIce <38312721+ForgottenIce@users.noreply.github.com>
2024-02-20 11:45:13 +0100

    Operation Result WIP
    This is goofy
    
    Co-Authored-By: Mads Hylleberg Nielsen <102622934+MalasataXD@users.noreply.github.com>
    Co-Authored-By: Marcus de Place <38247192+Thukena@users.noreply.github.com>

---
+++ src/Core/Tools/VIAEventAssociation.Core.Tools.OperationResult/Result.cs
@@ -0,0 +1,40 @@
+﻿namespace VIAEventAssociation.Core.Tools.OperationResult;
+
+public class Result
+{
+    private protected bool IsFailure = false;
+    private protected List<string> ErrorMessage = [];
+    private protected Result() {}
+    
+    public static Result Success() => new Result();
+
+    public static Result Failure(string errorMessage)
+    {
+        var result = new Result
+        {
+            IsFailure = true
+        };
+        result.ErrorMessage.Add(errorMessage);
+        return result;
+    }
+}
+
+public class Result<T> : Result
+{
+    public T Payload { get; init; }
+
+    public static Result<T> Success(T payload) => new()
+    {
+        Payload = payload
+    };
+
+    public new static Result<T> Failure(string errorMessage)
+    {
+        var result = new Result<T>()
+        {
+            IsFailure = true
+        };
+        result.ErrorMessage.Add(errorMessage);
+        return result;
+    }
+}
