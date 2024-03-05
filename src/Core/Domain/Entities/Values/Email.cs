718742913e28d6f0c1a7080f371d217e3ea054c6
Marcus de Place <marcus6001@live.dk>
2024-03-05 20:02:06 +0100

    Now validates Email when created

--- src/Core/Domain/Entities/Values/Email.cs
+++ src/Core/Domain/Entities/Values/Email.cs
@@ -1,4 +1,6 @@
 ﻿using VIAEventAssociation.Core.Tools.OperationResult;
+using VIAEventAssociation.Core.Tools.OperationResult.Errors;
+using VIAEventAssociation.Core.Tools.OperationResult.Errors.User;
 
 namespace VIAEventAssociation.Core.Domain.Entities.Values;
 
@@ -13,6 +15,89 @@ public class Email
     
     public static Result<Email> Create(string value)
     {
+        var errors = Validate(value);
+
+        if (errors.Count > 0)
+        {
+            return Result<Email>.Failure(errors.ToArray());
+        }
+        
         return Result<Email>.Success(new Email(value));
     }
+
+    private static List<Error> Validate(string value)
+    {
+        var errors = new List<Error>();
+
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            errors.Add(UserEmailError.EmailIsEmpty());
+            return errors;
+        }
+        
+        var email = value.ToLower();
+
+        if (!email.EndsWith("@via.dk", StringComparison.OrdinalIgnoreCase))
+        {
+            errors.Add(UserEmailError.EmailMustEndWith());
+            return errors;
+        }
+
+        var splitEmail = email.Split("@via.dk");
+        
+        if (splitEmail.Length > 2) //Check if the email contains @via.dk more than once
+        {
+            errors.Add(UserEmailError.EmailMustStartWith());
+            return errors;
+        }
+
+        var emailStart = splitEmail[0];
+
+        if (!emailStart.All(char.IsLetterOrDigit))
+        {
+            errors.Add(UserEmailError.EmailWithInvalidCharacters());
+            return errors;        
+        }
+        
+        if (!IsValidStartEmail(emailStart))
+        {
+            errors.Add(UserEmailError.EmailMustStartWith());
+            return errors;
+        }
+
+        return errors;
+    }
+
+    private static bool IsValidStartEmail(string emailStart)
+    {
+        var allLetters = emailStart.All(char.IsLetter);
+        var allDigits = emailStart.All(char.IsDigit);
+
+        if (!allLetters && !allDigits) //Contains both numbers and letters
+        {
+            return false;
+        }
+
+        if (emailStart.Any(char.IsLetter)) //Contains only letters
+        {
+            if (emailStart.Length is not (3 or 4))
+            {
+                return false;
+            }
+        }
+        else if (emailStart.Any(char.IsDigit)) //Contains only numbers
+        {
+            if (emailStart.Length != 6)
+            {
+                return false;
+            }
+        }
+        else //Contains no numbers or letters
+        {
+            return false;
+        }
+        
+        return true;
+    }
+    
 }
