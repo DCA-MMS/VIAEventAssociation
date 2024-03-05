1df060972a8357f87e7594c6c4becee5093dc58f
Marcus de Place <marcus6001@live.dk>
2024-03-05 18:28:01 +0100

    Created UnitTests for Email

---
+++ src/Core/Domain/Entities/Values/Email.cs
@@ -0,0 +1,18 @@
+﻿using VIAEventAssociation.Core.Tools.OperationResult;
+
+namespace VIAEventAssociation.Core.Domain.Entities.Values;
+
+public class Email
+{
+    public string Value { get; }
+
+    private Email(string value)
+    {
+        Value = value;
+    }
+    
+    public static Result<Email> Create(string value)
+    {
+        return Result<Email>.Success(new Email(value));
+    }
+}
