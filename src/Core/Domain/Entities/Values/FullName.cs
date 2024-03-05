8f16bf31b6740b7eb3ca19a135651965cdffd5b8
Marcus de Place <marcus6001@live.dk>
2024-03-05 15:14:37 +0100

    Created unit tests for FullName

---
+++ src/Core/Domain/Entities/Values/FullName.cs
@@ -0,0 +1,27 @@
+﻿using VIAEventAssociation.Core.Tools.OperationResult;
+
+namespace VIAEventAssociation.Core.Domain.Entities.Values;
+
+public class FullName
+{
+    public string FirstName { get; }
+    public string LastName { get; }
+
+
+    
+    private FullName(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    public static Result<FullName> Create(string firstName, string lastName)
+    {
+        return Result<FullName>.Success(new FullName(firstName, lastName));
+    }
+    
+    public override string ToString()
+    {
+        return $"{FirstName} {LastName}";
+    }
+}
---
+++ src/UnitTests/Values/User/FullNameTests.cs
@@ -0,0 +1,96 @@
+﻿using VIAEventAssociation.Core.Domain.Entities.Values;
+
+namespace Tests.Values.User;
+
+[TestFixture]
+public class FullNameTests
+{
+    [Test, Category("FullName")]
+    [TestCase("Bob", "Bobsen")]
+    public void Success_Create_FullName(string firstName, string lastName)
+    {
+        // Act
+        var result = FullName.Create(firstName, lastName);
+        Assert.Multiple(() =>
+        {
+            // Assert
+            Assert.That(result.IsFailure, Is.False);
+            Assert.That(result.Value.ToString(), Is.Not.Null);
+        });
+    }
+
+    [Test, Category("FullName")]
+    [TestCase("john", "johnson", ExpectedResult = "John Johnson")]
+    [TestCase("JOHN", "JOHNSON", ExpectedResult = "John Johnson")]
+    [TestCase("jOhN", "JOhnSoN", ExpectedResult = "John Johnson")]
+    [TestCase("jens", "andersen jensen olsen", ExpectedResult = "Andersen Jensen Olsen")]
+    [TestCase("JENS", "ANDERSEN JENSEN OLSEN", ExpectedResult = "Andersen Jensen Olsen")]
+    [TestCase("jeNs", "AndeSEn jENsEn, oLseN", ExpectedResult = "Andersen Jensen Olsen")]
+
+    public string Success_Create_FullName_With_Correct_Cased_Letters(string firstName, string lastName)
+    {
+        // Act
+        var result = FullName.Create(firstName, lastName);
+
+        // Assert
+        Assert.That(result.IsFailure, Is.False);
+        
+        return result.Value.ToString();
+    }
+    
+    [Test, Category("FullName")]
+    [TestCase("Bo", "johnson")]
+    [TestCase("Bo", "Bo")]
+    [TestCase("Bobsen", "Bo")]
+    [TestCase("Bob", "Bob")]
+    [TestCase("ThisNameIsTwentyFiveChara", "Bob")]
+    [TestCase("Bob", "ThisNameIsTwentyFourChara")]
+
+    public void Success_Create_FullName_With_Valid_Length(string firstName, string lastName)
+    {
+        // Act
+        var result = FullName.Create(firstName, lastName);
+
+        // Assert
+        Assert.That(result.IsFailure, Is.False);
+    }
+
+    
+    
+    [Test, Category("FullName")]
+    [TestCase("a", "Johnson")]
+    [TestCase("", "Johnson")]
+    [TestCase("John", "Johnson")]
+    [TestCase("John", "J")]
+    [TestCase("John", "")]
+    [TestCase("ThisNameIsTwentySixCharact", "Johnson")]
+    [TestCase("ThisNameIsTwentySixCharact", "ThisNameIsTwentySevenChara")]
+    [TestCase("ThisNameIsTwentySixCharact", "This Name Is TwentySeven Ch")]
+    [TestCase("jgklfdsjilgjrliajiltjdsiirllsdijrlisejilrjselirjseilrjslr", "irojesjrioe jsiorjeois rjoiesjroiesjoirjesior ioesjriojesoi rjesoirj oiesroisejrojseoirj seoijrs")]
+    public void Failure_Create_FullName_With_Invalid_Length(string firstName, string lastName)
+    {
+        // Act
+        var result = FullName.Create(firstName, lastName);
+
+        // Assert
+        Assert.That(result.IsFailure, Is.True);
+    }
+    
+    [Test, Category("FullName")]
+    [TestCase("12345", "12345")]
+    [TestCase("Hej1", "Johnson")]
+    [TestCase("John", "J0hnson")]
+    [TestCase("Jens#", "Jensen")]
+    [TestCase("Jens ", "Jensen")]
+    [TestCase("Jens Jens", "Jensen")]
+    [TestCase("Jens", "Jen-sen")]
+    [TestCase("Jens", "Jensen - Johnson")]
+    public void Failure_Create_FullName_With_Invalid_Characters(string firstName, string lastName)
+    {
+        // Act
+        var result = FullName.Create(firstName, lastName);
+
+        // Assert
+        Assert.That(result.IsFailure, Is.True);
+    }
+}
