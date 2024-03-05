0d6d0ae42fc8562417fff6543dc5957a131243c7
Marcus de Place <marcus6001@live.dk>
2024-03-05 16:55:21 +0100

    Now Validates FullName when created

--- src/Core/Domain/Entities/Values/FullName.cs
+++ src/Core/Domain/Entities/Values/FullName.cs
@@ -1,4 +1,7 @@
-﻿using VIAEventAssociation.Core.Tools.OperationResult;
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using VIAEventAssociation.Core.Tools.OperationResult;
+using VIAEventAssociation.Core.Tools.OperationResult.Errors;
 
 namespace VIAEventAssociation.Core.Domain.Entities.Values;
 
@@ -17,11 +20,55 @@ public class FullName
 
     public static Result<FullName> Create(string firstName, string lastName)
     {
-        return Result<FullName>.Success(new FullName(firstName, lastName));
+        var errors = Validate(firstName, false);
+        errors.AddRange(Validate(lastName, true));
+        
+        if (errors.Count > 0)
+        {
+            return Result<FullName>.Failure(errors.ToArray());
+        }
+        
+        var textInfo = new CultureInfo("en-US", false).TextInfo;
+
+        // Convert to title case
+        var formattedFirstName = textInfo.ToTitleCase(firstName.ToLower());
+        var formattedLastName = textInfo.ToTitleCase(lastName.ToLower());
+        
+        return Result<FullName>.Success(new FullName(formattedFirstName, formattedLastName));
     }
     
     public override string ToString()
     {
         return $"{FirstName} {LastName}";
+    }
+    
+    private static List<Error> Validate(string name, bool isLastName)
+    {
+        var errors = new List<Error>();
+    
+        //Validate FirstName
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            errors.Add(isLastName ? UserFullNameError.LastNameIsEmpty() : UserFullNameError.FirstNameIsEmpty());
+        }
+        
+        if (name.Length < 2)
+        {
+            errors.Add(isLastName ? UserFullNameError.LastNameIsTooShort() : UserFullNameError.FirstNameIsTooShort());
+        }
+        
+        if (name.Length > 25)
+        {
+            errors.Add(isLastName ? UserFullNameError.LastNameIsTooLong() : UserFullNameError.FirstNameIsTooLong());
+        }
+        
+        var namePattern = isLastName ? "^[a-zA-Z]+(?: [a-zA-Z]+)*$" : "^[a-zA-Z]+$"; 
+    
+        if (!Regex.IsMatch(name, namePattern))
+        {
+            errors.Add(isLastName ? UserFullNameError.LastNameIsInvalid() : UserFullNameError.FirstNameIsInvalid());
+        }
+        
+        return errors;
     }
 }
