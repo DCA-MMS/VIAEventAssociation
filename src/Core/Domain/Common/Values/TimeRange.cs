f393a1ee7384cf7a284c233c9543661084733c07
ForgottenIce <38312721+ForgottenIce@users.noreply.github.com>
2024-03-05 16:16:31 +0100

    TimeRange Value Object + Tests

---
+++ src/Core/Domain/Common/Values/TimeRange.cs
@@ -0,0 +1,38 @@
+﻿using VIAEventAssociation.Core.Tools.OperationResult;
+using VIAEventAssociation.Core.Tools.OperationResult.Errors;
+
+namespace VIAEventAssociation.Core.Domain.Common.Values;
+
+public class TimeRange
+{
+    public DateTime Start { get; private set; }
+    public DateTime End { get; private set; }
+
+    private TimeRange(DateTime start, DateTime end)
+    {
+        Start = start;
+        End = end;
+    }
+
+    public static Result<TimeRange> Create(DateTime start, DateTime end)
+    {
+        var result = Validate(start, end);
+        if (result.failure)
+        {
+            return Result<TimeRange>.Failure(result.errors.ToArray());
+        }
+
+        return new TimeRange(start, end);
+
+    }
+    
+    private static (bool failure, List<Error> errors) Validate(DateTime start, DateTime end)
+    {
+        var errors = new List<Error>();
+        if (start >= end)
+        {
+            errors.Add(TimeRangeError.EndBeforeStart());
+        }
+        return (errors.Count > 0, errors);
+    }
+}
