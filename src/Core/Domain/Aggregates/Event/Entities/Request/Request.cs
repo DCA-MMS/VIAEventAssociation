1fb86b3e8bc90788b70ff4fd36f07f1f360bc412
Marcus de Place <marcus6001@live.dk>
2024-03-08 09:50:41 +0100

    Created Request Entity

---
+++ src/Core/Domain/Aggregates/Event/Entities/Request/Request.cs
@@ -0,0 +1,34 @@
+﻿using VIAEventAssociation.Core.Domain.Entities.Request.Values;
+using VIAEventAssociation.Core.Domain.Entities.User;
+using VIAEventAssociation.Core.Tools.OperationResult;
+
+namespace VIAEventAssociation.Core.Domain.Aggregates.Event.Entities.Request;
+
+public class Request
+{
+    public User Guest { get; }
+    public RequestStatus Status { get; private set; }
+
+    private Request(User guest, RequestStatus status)
+    {
+        Guest = guest;
+        Status = status;
+    }
+
+    public static Result<Request> Create(User guest, RequestStatus status)
+    {
+        return Result<Request>.Success(new Request(guest, status));
+    }
+
+    public Result Approve()
+    {
+        Status = RequestStatus.Accepted;
+        return Result.Success();
+    }
+    
+    public Result Decline()
+    {
+        Status = RequestStatus.Rejected;
+        return Result.Success();
+    }
+}
---
+++ src/Core/Domain/Aggregates/Event/Entities/Request/Values/RequestStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace VIAEventAssociation.Core.Domain.Entities.Request.Values;
+
+public enum RequestStatus
+{
+    Pending = 0,
+    Accepted = 1,
+    Rejected = 2
+}
