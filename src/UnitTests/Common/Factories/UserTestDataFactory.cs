cf3652cd481278b3fcec2d1cf248d2163ba5b05d
Marcus de Place <marcus6001@live.dk>
2024-03-09 12:09:12 +0100

    Created UserTestDataFactory

---
+++ src/UnitTests/Common/Factories/UserTestDataFactory.cs
@@ -0,0 +1,12 @@
+﻿using VIAEventAssociation.Core.Domain.Entities.User;
+using VIAEventAssociation.Core.Domain.Entities.User.Values;
+
+namespace Tests.Common.Factories;
+
+public static class UserTestDataFactory
+{
+    public static User ValidUser()
+    {
+        return User.Create(FullName.Create("John", "Johnson"), Email.Create("John@via.dk")).Value;
+    }
+}
