2651f4dd1d0332930b4ba0a04d633c1963172388
Marcus de Place <marcus6001@live.dk>
2024-03-09 12:09:29 +0100

    Created Request UnitTests

---
+++ src/UnitTests/Values/RequestTests/RequestTests.cs
@@ -0,0 +1,72 @@
+﻿using Tests.Common.Factories;
+using VIAEventAssociation.Core.Domain.Aggregates.Event.Entities.Request;
+using VIAEventAssociation.Core.Domain.Entities.Request.Values;
+using VIAEventAssociation.Core.Domain.Entities.User.Values;
+using User = VIAEventAssociation.Core.Domain.Entities.User.User; 
+
+namespace Tests.Values.RequestTests;
+
+[TestFixture]
+public class RequestTests
+{
+    private readonly User _user = UserTestDataFactory.ValidUser();
+
+    [Test, Category("Request")]
+    [TestCase(RequestStatus.Pending)]
+    [TestCase(RequestStatus.Rejected)]
+    [TestCase(RequestStatus.Accepted)]
+    public void Success_Create_Request(RequestStatus status)
+    {
+        // Arrange
+        var request = Request.Create(_user, status);
+            
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(request.IsFailure, Is.False);
+            Assert.That(request.Value.Guest, Is.EqualTo(_user));
+            Assert.That(request.Value.Status, Is.EqualTo(status));
+        });
+    }
+    
+    [Test, Category("Request")]
+    [TestCase(RequestStatus.Pending)]
+    public void Success_Approve_Pending_Request(RequestStatus status)
+    {
+        // Arrange
+        var request = Request.Create(_user, status);
+            
+        // Act
+        var result = request.Value.Approve();
+        
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(request.IsFailure, Is.False);
+            Assert.That(result.IsFailure, Is.False);
+            Assert.That(request.Value.Guest, Is.EqualTo(_user));
+            Assert.That(request.Value.Status, Is.EqualTo(RequestStatus.Accepted));
+        });
+    }
+    
+        
+    [Test, Category("Request")]
+    [TestCase(RequestStatus.Pending)]
+    public void Success_Decline_Pending_Request(RequestStatus status)
+    {
+        // Arrange
+        var request = Request.Create(_user, status);
+            
+        // Act
+        var result = request.Value.Decline();
+        
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(request.IsFailure, Is.False);
+            Assert.That(result.IsFailure, Is.False);
+            Assert.That(request.Value.Guest, Is.EqualTo(_user));
+            Assert.That(request.Value.Status, Is.EqualTo(RequestStatus.Rejected));
+        });
+    }
+}
