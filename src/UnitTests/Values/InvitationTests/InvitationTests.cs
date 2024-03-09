32845bcc8db08ed62ea7e1fc8bc63148fab27305
Marcus de Place <marcus6001@live.dk>
2024-03-09 12:57:15 +0100

    Created UnitTests for Invitation

---
+++ src/UnitTests/Values/InvitationTests/InvitationTests.cs
@@ -0,0 +1,70 @@
+﻿using Tests.Common.Factories;
+using VIAEventAssociation.Core.Domain.Aggregates.Event.Entities.Invitation;
+using VIAEventAssociation.Core.Domain.Aggregates.Event.Entities.Invitation.Values;
+using VIAEventAssociation.Core.Domain.Entities.User;
+
+namespace Tests.Values.InvitationTests;
+
+[TestFixture]
+public class InvitationTests
+{
+    private readonly User _user = UserTestDataFactory.ValidUser();
+
+    [Test, Category("Invitation")]
+    [TestCase(InvitationStatus.Pending)]
+    [TestCase(InvitationStatus.Rejected)]
+    [TestCase(InvitationStatus.Accepted)]
+    public void Success_Create_Invitation(InvitationStatus status)
+    {
+        // Arrange
+        var invitation = Invitation.Create(_user, status);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(invitation.IsFailure, Is.False);
+            Assert.That(invitation.Value.Guest, Is.EqualTo(_user));
+            Assert.That(invitation.Value.Status, Is.EqualTo(status));
+        });
+    }
+
+    [Test, Category("Invitation")]
+    [TestCase(InvitationStatus.Pending)]
+    public void Success_Accept_Pending_Invitation(InvitationStatus status)
+    {
+        // Arrange
+        var invitation = Invitation.Create(_user, status);
+
+        // Act
+        var result = invitation.Value.Accept();
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(invitation.IsFailure, Is.False);
+            Assert.That(result.IsFailure, Is.False);
+            Assert.That(invitation.Value.Guest, Is.EqualTo(_user));
+            Assert.That(invitation.Value.Status, Is.EqualTo(InvitationStatus.Accepted));
+        });
+    }
+
+    [Test, Category("Invitation")]
+    [TestCase(InvitationStatus.Pending)]
+    public void Success_Decline_Pending_Invitation(InvitationStatus status)
+    {
+        // Arrange
+        var invitation = Invitation.Create(_user, status);
+
+        // Act
+        var result = invitation.Value.Decline();
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(invitation.IsFailure, Is.False);
+            Assert.That(result.IsFailure, Is.False);
+            Assert.That(invitation.Value.Guest, Is.EqualTo(_user));
+            Assert.That(invitation.Value.Status, Is.EqualTo(InvitationStatus.Rejected));
+        });
+    }
+}
