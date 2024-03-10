db6c71bf588f40ff090bc3707dae5a9b35db4847
Marcus de Place <marcus6001@live.dk>
2024-03-10 14:58:49 +0100

    Added validation for RequestReason

--- src/Core/Domain/Aggregates/Event/Entities/Request/Values/RequestReason.cs
+++ src/Core/Domain/Aggregates/Event/Entities/Request/Values/RequestReason.cs
@@ -1,4 +1,6 @@
 ﻿using VIAEventAssociation.Core.Tools.OperationResult;
+using VIAEventAssociation.Core.Tools.OperationResult.Errors;
+using VIAEventAssociation.Core.Tools.OperationResult.Errors.Event;
 
 namespace VIAEventAssociation.Core.Domain.Aggregates.Event.Entities.Request.Values;
 
@@ -13,6 +15,25 @@ public class RequestReason
 
     public static Result<RequestReason> Create(string value)
     {
+        var errors = Validate(value);
+
+        if (errors.Count > 0)
+        {
+            return Result<RequestReason>.Failure(errors.ToArray());
+        }
+        
         return Result<RequestReason>.Success(new RequestReason(value));
+    }
+
+    private static List<Error> Validate(string value)
+    {
+        var errors = new List<Error>();
+
+        if (value.Length > 250)
+        {
+            errors.Add(EventRequestReasonError.RequestReasonIsTooLong());
+        }
+        
+        return errors;
     }
 }
