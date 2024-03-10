a0f33b81bda04fd269e5accaa09a1cb7ccf81f81
Marcus de Place <marcus6001@live.dk>
2024-03-10 14:11:24 +0100

    Created RequestReason ValueObject

---
+++ src/Core/Domain/Aggregates/Event/Entities/Request/Values/RequestReason.cs
@@ -0,0 +1,18 @@
+﻿using VIAEventAssociation.Core.Tools.OperationResult;
+
+namespace VIAEventAssociation.Core.Domain.Aggregates.Event.Entities.Request.Values;
+
+public class RequestReason
+{
+    public string Value { get; }
+
+    private RequestReason(string value)
+    {
+        Value = value;
+    }
+
+    public static Result<RequestReason> Create(string value)
+    {
+        return Result<RequestReason>.Success(new RequestReason(value));
+    }
+}
