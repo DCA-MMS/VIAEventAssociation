34ad61dc440508e1084882608a6593c47c54110e
ForgottenIce <38312721+ForgottenIce@users.noreply.github.com>
2024-03-31 00:09:07 +0100

    MakePrivateCommand & MakePrivateHandler

---
+++ src/Core/Application/Features/EventHandlers/MakePrivateHandler.cs
@@ -0,0 +1,36 @@
+﻿using Application.AppEntry.Commands.EventCommands;
+using Application.AppEntry.Interfaces;
+using VIAEventAssociation.Core.Domain.Aggregates.Event;
+using VIAEventAssociation.Core.Domain.Common;
+using VIAEventAssociation.Core.Tools.OperationResult;
+using VIAEventAssociation.Core.Tools.OperationResult.Errors;
+
+namespace Application.Features.EventHandlers;
+
+internal class MakePrivateHandler : ICommandHandler<MakePrivateCommand>
+{
+    private readonly IEventRepository _repository;
+    private readonly IUnitOfWork _unitOfWork;
+    
+    internal MakePrivateHandler(IEventRepository repository, IUnitOfWork uow) => (this._repository, this._unitOfWork) = (repository, uow);
+    
+    public async Task<Result> HandleAsync(MakePrivateCommand command)
+    {
+        var @event = await _repository.GetByIdAsync(command.Id);
+        
+        if (@event == null)
+        {
+            return Result.Failure(RepositoryError.ItemNotFound());
+        }
+        
+        var result = @event.MakePrivate();
+        
+        if (result.IsFailure)
+        {
+            return result;
+        }
+        
+        await _unitOfWork.SaveChangesAsync();
+        return Result.Success();
+    }
+}
