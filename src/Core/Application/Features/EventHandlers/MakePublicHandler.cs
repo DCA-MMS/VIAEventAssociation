c53b8254db043ba8822eb4c956ddc5ddbcf8145a
ForgottenIce <38312721+ForgottenIce@users.noreply.github.com>
2024-03-31 00:02:27 +0100

    MakePublicCommand & MakePublicHandler

---
+++ src/Core/Application/Features/EventHandlers/MakePublicHandler.cs
@@ -0,0 +1,36 @@
+﻿using Application.AppEntry.Commands.EventCommands;
+using Application.AppEntry.Interfaces;
+using VIAEventAssociation.Core.Domain.Aggregates.Event;
+using VIAEventAssociation.Core.Domain.Common;
+using VIAEventAssociation.Core.Tools.OperationResult;
+using VIAEventAssociation.Core.Tools.OperationResult.Errors;
+
+namespace Application.Features.EventHandlers;
+
+internal class MakePublicHandler : ICommandHandler<MakePublicCommand>
+{
+    private readonly IEventRepository _repository;
+    private readonly IUnitOfWork _unitOfWork;
+    
+    internal MakePublicHandler(IEventRepository repository, IUnitOfWork uow) => (this._repository, this._unitOfWork) = (repository, uow);
+    
+    public async Task<Result> HandleAsync(MakePublicCommand command)
+    {
+        var @event = await _repository.GetByIdAsync(command.Id);
+        
+        if (@event == null)
+        {
+            return Result.Failure(RepositoryError.ItemNotFound());
+        }
+        
+        var result = @event.MakePublic();
+        
+        if (result.IsFailure)
+        {
+            return result;
+        }
+        
+        await _unitOfWork.SaveChangesAsync();
+        return Result.Success();
+    }
+}
