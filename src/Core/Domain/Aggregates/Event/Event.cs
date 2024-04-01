137cfd34f636dbb827e5dde9feb30c962f6ebece
Marcus de Place <marcus6001@live.dk>
2024-04-01 10:22:45 +0200

    Created Command + Handler for UC 15 - DeclineInvitation

--- src/Core/Domain/Aggregates/Event/Event.cs
+++ src/Core/Domain/Aggregates/Event/Event.cs
@@ -385,7 +385,7 @@ public class Event
     {
         var errors = new List<Error>();
         
-        var invitation = Invitations.FirstOrDefault(x => x.GuestId == userId);
+        var invitation = Invitations.FirstOrDefault(x => x.GuestId.Value == userId.Value);
         if (invitation == null)
         {
             errors.Add(EventInvitationError.InvitationDeclineToGuestNotInvited());
---
+++ src/UnitTests/Features/Event/UC15-DeclineInvitation/DeclineInvitationCommandTest.cs
@@ -0,0 +1,29 @@
+﻿using Application.AppEntry.Commands.EventCommands;
+using Tests.Fakes;
+
+namespace Tests.Features.Event.UC15_DeclineInvitation;
+
+public class DeclineInvitationCommandTest
+{
+    private FakeEventRepository EventRepository { get; } = new();   
+    private FakeUserRepository UserRepository { get; } = new();
+    
+    // # S1
+    [Test]
+    public void Create_DeclineInvitationCommand_Success()
+    {
+        // Arrange
+        var @event = EventRepository.Events.First();
+        var user = UserRepository.Users.First();
+
+        // Act
+        var command = DeclineInvitationCommand.Create(@event.Id.Value.ToString(), user.Id.Value.ToString());
+      
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(command.Value.EventId.Value, Is.EqualTo(@event.Id.Value));
+            Assert.That(command.Value.UserId.Value, Is.EqualTo(user.Id.Value));
+        });
+    }
+}
