e93569b409d7fc4b15a2e7e7233d69145b7227cb
ForgottenIce <38312721+ForgottenIce@users.noreply.github.com>
2024-04-01 14:50:55 +0200

    ChangeCapacityCommand & ChangeCapacityHandler

---
+++ src/Core/Application/AppEntry/Commands/EventCommands/ChangeCapacityCommand.cs
@@ -0,0 +1,35 @@
+﻿using VIAEventAssociation.Core.Domain.Aggregates.Event;
+using VIAEventAssociation.Core.Domain.Aggregates.Event.Values;
+using VIAEventAssociation.Core.Domain.Common.Values;
+using VIAEventAssociation.Core.Tools.OperationResult;
+using VIAEventAssociation.Core.Tools.OperationResult.Errors;
+
+namespace Application.AppEntry.Commands.EventCommands;
+
+public class ChangeCapacityCommand
+{
+    public Id<Event> Id { get; }
+    public EventCapacity Capacity { get; }
+    
+    private ChangeCapacityCommand(Id<Event> id, EventCapacity capacity)
+    {
+        Id = id;
+        Capacity = capacity;
+    }
+    
+public static Result<ChangeCapacityCommand> Create(string id, int capacity)
+    {
+        var eventIdResult = Id<Event>.FromString(id);
+        var capacityResult = EventCapacity.Create(capacity);
+        
+        if (eventIdResult.IsFailure || capacityResult.IsFailure)
+        {
+            List<Error> errors = [];
+            errors.AddRange(eventIdResult.Errors);
+            errors.AddRange(capacityResult.Errors);
+            return Result<ChangeCapacityCommand>.Failure(errors.ToArray());
+        }
+        
+        return Result<ChangeCapacityCommand>.Success(new ChangeCapacityCommand(eventIdResult, capacityResult));
+    }
+}
---
+++ src/Core/Application/Features/EventHandlers/ChangeCapacityHandler.cs
@@ -0,0 +1,36 @@
+﻿using Application.AppEntry.Commands.EventCommands;
+using Application.AppEntry.Interfaces;
+using VIAEventAssociation.Core.Domain.Aggregates.Event;
+using VIAEventAssociation.Core.Domain.Common;
+using VIAEventAssociation.Core.Tools.OperationResult;
+using VIAEventAssociation.Core.Tools.OperationResult.Errors;
+
+namespace Application.Features.EventHandlers;
+
+public class ChangeCapacityHandler : ICommandHandler<ChangeCapacityCommand>
+{
+    private readonly IEventRepository _repository;
+    private readonly IUnitOfWork _unitOfWork;
+    
+    internal ChangeCapacityHandler(IEventRepository repository, IUnitOfWork uow) => (_repository, _unitOfWork) = (repository, uow);
+    
+    public async Task<Result> HandleAsync(ChangeCapacityCommand command)
+    {
+        var @event = await _repository.GetByIdAsync(command.Id);
+        
+        if (@event == null)
+        {
+            return Result.Failure(RepositoryError.ItemNotFound());
+        }
+        
+        var result = @event.ChangeCapacity(command.Capacity);
+        
+        if (result.IsFailure)
+        {
+            return result;
+        }
+        
+        await _unitOfWork.SaveChangesAsync();
+        return Result.Success();
+    }
+}
