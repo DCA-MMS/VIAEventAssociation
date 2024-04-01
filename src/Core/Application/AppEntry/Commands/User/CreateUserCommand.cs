359c92f8d82da6bbc926a9366511ebe28387a1bd
ForgottenIce <38312721+ForgottenIce@users.noreply.github.com>
2024-04-01 15:16:52 +0200

    CreateUserCommand & CreateUserHandler

---
+++ src/Core/Application/AppEntry/Commands/User/CreateUserCommand.cs
@@ -0,0 +1,33 @@
+﻿using VIAEventAssociation.Core.Domain.Aggregates.Users.Values;
+using VIAEventAssociation.Core.Tools.OperationResult;
+using VIAEventAssociation.Core.Tools.OperationResult.Errors;
+
+namespace Application.AppEntry.Commands.User;
+
+public class CreateUserCommand
+{
+    public FullName FullName { get; }
+    public Email Email { get; }
+    
+    private CreateUserCommand(FullName fullName, Email email)
+    {
+        FullName = fullName;
+        Email = email;
+    }
+    
+    public static Result<CreateUserCommand> Create(string firstName, string lastName, string email)
+    {
+        var fullNameResult = FullName.Create(firstName, lastName);
+        var emailResult = Email.Create(email);
+        
+        if (fullNameResult.IsFailure || emailResult.IsFailure)
+        {
+            List<Error> errors = [];
+            errors.AddRange(fullNameResult.Errors);
+            errors.AddRange(emailResult.Errors);
+            return Result<CreateUserCommand>.Failure(errors.ToArray());
+        }
+        
+        return Result<CreateUserCommand>.Success(new CreateUserCommand(fullNameResult, emailResult));
+    }
+}
