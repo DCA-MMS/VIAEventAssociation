b22f2b68f29aa0fe9888074278aaa3d63cec01df
ForgottenIce <38312721+ForgottenIce@users.noreply.github.com>
2024-04-01 14:59:55 +0200

    ActivateCommand & ActivateHandler

---
+++ src/Core/Application/AppEntry/Commands/EventCommands/ActivateCommand.cs
@@ -0,0 +1,27 @@
+﻿using VIAEventAssociation.Core.Domain.Aggregates.Event;
+using VIAEventAssociation.Core.Domain.Common.Values;
+using VIAEventAssociation.Core.Tools.OperationResult;
+
+namespace Application.AppEntry.Commands.EventCommands;
+
+public class ActivateCommand
+{
+    public Id<Event> Id { get; }
+    
+    private ActivateCommand(Id<Event> id)
+    {
+        Id = id;
+    }
+    
+    public static Result<ActivateCommand> Create(string id)
+    {
+        var eventIdResult = Id<Event>.FromString(id);
+        
+        if (eventIdResult.IsFailure)
+        {
+            return Result<ActivateCommand>.Failure(eventIdResult.Errors.ToArray());
+        }
+        
+        return Result<ActivateCommand>.Success(new ActivateCommand(eventIdResult));
+    }
+}
