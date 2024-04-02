2c1c832b1272c6a83320fe09366e9441d42f7bde
ForgottenIce <38312721+ForgottenIce@users.noreply.github.com>
2024-04-02 11:17:44 +0200

    Logger decorator

---
+++ Logger/ILogger.cs
@@ -0,0 +1,6 @@
+﻿namespace Logger;
+
+public interface ILogger
+{
+    Task LogAsync(DateTime time, string operation, string details);
+}
---
+++ src/Core/Application/AppEntry/Decorators/LogDecorator.cs
@@ -0,0 +1,18 @@
+﻿using Application.AppEntry.Dispatcher;
+using Logger;
+using VIAEventAssociation.Core.Tools.OperationResult;
+
+namespace Application.AppEntry.Decorators;
+
+public class LogDecorator(ICommandDispatcher next) : ICommandDispatcher
+{
+    public async Task<Result> DispatchAsync<TCommand>(TCommand command)
+    {
+        Result result = await next.DispatchAsync(command);
+        
+        FileLogger logger = new FileLogger("VEA.log");
+        await logger.LogAsync(DateTime.Now, nameof(command), result.IsFailure ? "Failed" : "Success");
+
+        return result;
+    }
+}
