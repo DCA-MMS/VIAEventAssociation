a124566a0bb278ad7068049b1e084d17509a01c3
thukena <marcus6001@live.dk>
2024-04-09 11:59:41 +0200

    created EfcRepositoryBase

---
+++ src/Infrastructure/EfcDmPersistence/EfcRepositoryBase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VIAEventAssociation.Core.Domain.Common.Values;
+using VIAEventAssociation.Core.Domain.Repositories;
+
+namespace VIAEventAssociation.Infrastructure.EfcDmPersistence;
+
+public abstract class EfcRepositoryBase<TAgg>(DbContext context) : IRepository<TAgg> where TAgg : class
+{
+    public virtual async Task AddAsync(TAgg aggregate)
+    {
+        await context.Set<TAgg>().AddAsync(aggregate);
+    }
+
+    public virtual async Task<TAgg?> GetByIdAsync(Id<TAgg> id)
+    {
+        return await context.Set<TAgg>().FindAsync(id);
+    }
+}
