56402d1f02f0e47137c0721fd896b52f52d5fc92
thukena <marcus6001@live.dk>
2024-04-09 10:56:36 +0200

    Efc project init

---
+++ src/Infrastructure/EfcDmPersistence/EfcDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VIAEventAssociation.Infrastructure.EfcDmPersistence;
+
+public class EfcDbContext(DbContextOptions options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(EfcDbContext).Assembly);
+    }
+}
