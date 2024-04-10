b98856a793397219b5780357826c9081b367c5fc
ForgottenIce <38312721+ForgottenIce@users.noreply.github.com>
2024-04-10 12:22:26 +0200

    InvitationId configuration & converter

---
+++ src/Infrastructure/EfcDmPersistence/Configurations/InvitationConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VIAEventAssociation.Core.Domain.Aggregates.Event.Entities.Invitation;
+using VIAEventAssociation.Core.Domain.Aggregates.Event.Entities.Invitation.Values;
+
+namespace VIAEventAssociation.Infrastructure.EfcDmPersistence.Configurations;
+
+public class InvitationConfiguration : IEntityTypeConfiguration<Invitation>
+{
+    public void Configure(EntityTypeBuilder<Invitation> builder)
+    {
+        builder.HasKey(i => i.Id);
+        builder.Property(i => i.Id)
+            .HasConversion(
+                id => id.Value,
+                dbValue => InvitationId.FromGuid(dbValue)
+            );
+    }
+}
