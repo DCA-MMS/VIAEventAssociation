9e7e6e358c28a3817689435439939a7717860dae
ForgottenIce <38312721+ForgottenIce@users.noreply.github.com>
2024-04-12 17:46:36 +0200

    UserId configuration & converter

--- src/Infrastructure/EfcDmPersistence/Configurations/UserConfiguration.cs
+++ src/Infrastructure/EfcDmPersistence/Configurations/UserConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using VIAEventAssociation.Core.Domain.Aggregates.Users;
+using VIAEventAssociation.Core.Domain.Aggregates.Users.Values;
 
 namespace VIAEventAssociation.Infrastructure.EfcDmPersistence.Configurations;
 
@@ -8,6 +9,11 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
 {
     public void Configure(EntityTypeBuilder<User> builder)
     {
-        
+        builder.HasKey(u => u.Id);
+        builder.Property(u => u.Id)
+            .HasConversion(
+                id => id.Value,
+                dbValue => UserId.FromGuid(dbValue)
+            );
     }
 }
