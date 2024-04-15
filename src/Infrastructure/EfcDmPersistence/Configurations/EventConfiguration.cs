179e29599b8f003b4b0044759ba2b7aa8d8bce00
ForgottenIce <38312721+ForgottenIce@users.noreply.github.com>
2024-04-15 11:56:56 +0200

    Event Duration EFC Configuration

--- src/Infrastructure/EfcDmPersistence/Configurations/EventConfiguration.cs
+++ src/Infrastructure/EfcDmPersistence/Configurations/EventConfiguration.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using VIAEventAssociation.Core.Domain.Aggregates.Event;
 using VIAEventAssociation.Core.Domain.Aggregates.Event.Values;
+using VIAEventAssociation.Core.Domain.Common.Values;
 
 namespace VIAEventAssociation.Infrastructure.EfcDmPersistence.Configurations;
 
@@ -36,6 +37,10 @@ public class EventConfiguration : IEntityTypeConfiguration<Event>
                 propBuilder.Property(ec => ec.Value).HasColumnName("Capacity");
             }
         );
-        
+        builder.OwnsOne<TimeRange>("Duration", t =>
+        {
+            t.Property(t => t.Start).HasColumnName("DurationStart");
+            t.Property(t => t.End).HasColumnName("DurationEnd");
+        });
     }
 }
