49e496a9de9470becfbbe6a6b375ab67a24ba7d9
ForgottenIce <38312721+ForgottenIce@users.noreply.github.com>
2024-04-16 11:16:40 +0200

    Database scaffold to EfcQueries

---
+++ src/Infrastructure/EfcQueries/Invitation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VIAEventAssociation.Infrastructure.EfcQueries;
+
+public partial class Invitation
+{
+    public string Id { get; set; } = null!;
+
+    public string GuestId { get; set; } = null!;
+
+    public int Status { get; set; }
+
+    public string? EventId { get; set; }
+
+    public virtual Event? Event { get; set; }
+
+    public virtual User Guest { get; set; } = null!;
+}
