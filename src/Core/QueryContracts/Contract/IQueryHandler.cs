c996a2d4467e03447d3260b273d228669f534dbd
Marcus de Place <marcus6001@live.dk>
2024-04-22 21:05:56 +0200

    Created UserProfilePage query

--- src/Core/QueryContracts/Contract/IQueryHandler.cs
+++ src/Core/QueryContracts/Contract/IQueryHandler.cs
@@ -1,6 +1,8 @@
-﻿namespace VIAEventAssociation.Core.QueryContracts.Contract;
+﻿using VIAEventAssociation.Core.Tools.OperationResult;
+
+namespace VIAEventAssociation.Core.QueryContracts.Contract;
 
 public interface IQueryHandler<in TQuery, TAnswer> where TQuery : IQuery<TAnswer>
 {
-    
+    Task<TAnswer> HandleAsync(TQuery query);
 }
