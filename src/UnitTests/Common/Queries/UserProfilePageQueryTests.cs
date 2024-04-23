44c2c59128fc2713c68056c4b6deacb023781744
thukena <marcus6001@live.dk>
2024-04-23 11:54:42 +0200

    WIP Test for queries

---
+++ src/UnitTests/Common/Queries/UserProfilePageQueryTests.cs
@@ -0,0 +1,31 @@
+﻿using Application.AppEntry.Commands.EventCommands;
+using Application.AppEntry.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Tests.Fakes.CommandHandlers.EventHandlers;
+using VIAEventAssociation.Core.QueryContracts.Contract;
+using VIAEventAssociation.Core.QueryContracts.Queries;
+using VIAEventAssociation.Core.QueryContracts.QueryDispatching;
+using VIAEventAssociation.Infrastructure.EfcDmPersistence;
+using VIAEventAssociation.Infrastructure.EfcQueries.Queries;
+
+namespace Tests.Common.Queries;
+
+public class UserProfilePageQueryTests
+{
+    [Test]
+    public void PersonalProfilePage_Query_Json()
+    {
+        // Arrange
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddScoped<IQueryHandler<UserProfilePage.Query,UserProfilePage.Answer>, ProfilePageQueryHandler>();
+        IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+        
+        var query = new UserProfilePage.Query("86a87d88-93bf-404f-a5b0-5d60a5cf19b7");
+        QueryDispatcher queryDispatcher = new(serviceProvider);
+        var answer = queryDispatcher.DispatchAsync(query);
+        
+        Console.WriteLine(answer.Result);
+    }
+    
+    
+}
