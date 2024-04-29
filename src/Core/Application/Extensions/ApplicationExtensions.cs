c75bb4773f4fb8c1e410c65ac79a36acc872debf
ForgottenIce <38312721+ForgottenIce@users.noreply.github.com>
2024-04-29 14:56:50 +0200

    WebAPI init & Extensions prepared

---
+++ src/Core/Application/Extensions/ApplicationExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Extensions;
+
+public static class ApplicationExtensions
+{
+    public static void RegisterHandlers(this IServiceCollection services)
+    {
+        //TODO: Register all command handlers
+    }
+    
+    public static void RegisterDispatcher(this IServiceCollection services)
+    {
+        //TODO: Register dispatcher
+    }
+}
