228588476991ae6e45e2a78c269deb80590e5e4e
Mads Hylleberg Nielsen <102622934+MalasataXD@users.noreply.github.com>
2024-05-10 12:43:19 +0200

    ProfilePageQueryHandler now works + Test ;)

--- src/UnitTests/Common/Queries/UserProfilePageQueryTests.cs
+++ src/UnitTests/Common/Queries/UserProfilePageQueryTests.cs
@@ -1,12 +1,14 @@
-﻿using Application.AppEntry.Commands.EventCommands;
-using Application.AppEntry.Interfaces;
+﻿
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Tests.Fakes.CommandHandlers.EventHandlers;
+using Newtonsoft.Json;
+using Tests.Common.DatabaseTest;
 using VIAEventAssociation.Core.QueryContracts.Contract;
 using VIAEventAssociation.Core.QueryContracts.Queries;
 using VIAEventAssociation.Core.QueryContracts.QueryDispatching;
 using VIAEventAssociation.Infrastructure.EfcDmPersistence;
 using VIAEventAssociation.Infrastructure.EfcQueries.Queries;
+using VIAEventAssociation.Infrastructure.EfcQueries.Scaffold;
 
 namespace Tests.Common.Queries;
 
@@ -17,15 +19,33 @@ public class UserProfilePageQueryTests
     {
         // Arrange
         var serviceCollection = new ServiceCollection();
-        serviceCollection.AddScoped<IQueryHandler<UserProfilePage.Query,UserProfilePage.Answer>, ProfilePageQueryHandler>();
+        
+ 
+        // Add the DbContext
+        serviceCollection.AddScoped<VeadatabaseContext>(_ => SetUpReadContext().Seed());
+        
+        // Add the QueryHandler.
+        serviceCollection.AddScoped<IQueryHandler<UserProfilePage.Query,UserProfilePage.Answer>,  ProfilePageQueryHandler>();
         IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
         
+        // Act - Dispatch the query
         var query = new UserProfilePage.Query("86a87d88-93bf-404f-a5b0-5d60a5cf19b7");
         QueryDispatcher queryDispatcher = new(serviceProvider);
         var answer = queryDispatcher.DispatchAsync(query);
         
-        Console.WriteLine(answer.Result);
+        // Assert
+        Console.WriteLine(JsonConvert.SerializeObject(answer.Result));
     }
-    
-    
+
+    private static VeadatabaseContext SetUpReadContext()
+    {
+        var testDbName = "Test.db";
+        DbContextOptionsBuilder<VeadatabaseContext> optionsBuilder = new();
+        optionsBuilder.UseSqlite($"Data Source = {testDbName}");
+        VeadatabaseContext context = new(optionsBuilder.Options);
+        context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
+
+        return context;
+    }
 }
