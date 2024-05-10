ad82ea07265ff362f479f6eb500679f08f2402e2
ForgottenIce <38312721+ForgottenIce@users.noreply.github.com>
2024-05-10 14:36:22 +0200

    Implement repositories
    Implemented the repositories that we somehow forgor to implement

---
+++ src/Infrastructure/EfcDmPersistence/Repositories/EfcEventRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VIAEventAssociation.Core.Domain.Aggregates.Event;
+using VIAEventAssociation.Core.Domain.Common.Values;
+
+namespace VIAEventAssociation.Infrastructure.EfcDmPersistence.Repositories;
+
+public class EfcEventRepository(DbContext dbContext) : EfcRepositoryBase<Event>(dbContext), IEventRepository
+{
+    public override Task<Event?> GetByIdAsync(Id<Event> id)
+    {
+        return dbContext.Set<Event>()
+            .Include(e => e.Invitations)
+            .Include(e => e.Participants)
+            .FirstOrDefaultAsync(e => e.Id.Value == id.Value);
+    }
+}
---
+++ src/Infrastructure/EfcDmPersistence/Repositories/EfcUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VIAEventAssociation.Core.Domain.Aggregates.Users;
+using VIAEventAssociation.Core.Domain.Common.Values;
+
+namespace VIAEventAssociation.Infrastructure.EfcDmPersistence.Repositories;
+
+public class EfcUserRepository(DbContext dbContext) : EfcRepositoryBase<User>(dbContext), IUserRepository
+{
+    public override Task<User?> GetByIdAsync(Id<User> id)
+    {
+        return dbContext.Set<User>().FirstOrDefaultAsync(u => u.Id.Value == id.Value);
+    }
+}
