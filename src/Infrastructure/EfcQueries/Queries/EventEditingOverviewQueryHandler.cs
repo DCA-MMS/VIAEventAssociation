021539b9bf55eba6dc2843b17b27b1174e726965
Mads Hylleberg Nielsen <102622934+MalasataXD@users.noreply.github.com>
2024-06-03 09:51:18 +0200

    Revert "Assignment 8 (DONE) to Assignment 9."

--- src/Infrastructure/EfcQueries/Queries/EventEditingOverviewQueryHandler.cs
+++ src/Infrastructure/EfcQueries/Queries/EventEditingOverviewQueryHandler.cs
@@ -3,39 +3,34 @@ using VIAEventAssociation.Core.Domain.Aggregates.Event.Values;
 using VIAEventAssociation.Core.QueryContracts.Contract;
 using VIAEventAssociation.Core.QueryContracts.Queries;
 using VIAEventAssociation.Infrastructure.EfcDmPersistence;
-using VIAEventAssociation.Infrastructure.EfcQueries.Scaffold;
 
 namespace VIAEventAssociation.Infrastructure.EfcQueries.Queries;
 
-public class EventEditingOverviewQueryHandler(VeadatabaseContext context) : IQueryHandler<EventEditingOverview.Query, EventEditingOverview.Answer>
+public class EventEditingOverviewQueryHandler(EfcDbContext context) : IQueryHandler<EventEditingOverview.Query, EventEditingOverview.Answer>
 {
     public async Task<EventEditingOverview.Answer> HandleAsync(EventEditingOverview.Query query)
     {
-        // #1: Fetch all events that are in draft status
-        var eventDrafts = await context.Events
-            .Where(e => e.Status == (int) EventStatus.Draft)
+        var drafts = await context.Events
+            .Where(e => e.Status == EventStatus.Draft)
             .Select(e => new EventEditingOverview.Event(
-                e.Id,
+                e.Id.Value.ToString(),
                 e.Title
             )).ToListAsync();
-        
-        // #2: Fetch all events that are ready
-        var readyEvents = await context.Events
-            .Where(e => e.Status == (int) EventStatus.Ready)
+
+        var ready = await context.Events
+            .Where(e => e.Status == EventStatus.Ready)
             .Select(e => new EventEditingOverview.Event(
-                e.Id,
+                e.Id.Value.ToString(),
                 e.Title
             )).ToListAsync();
-        
-        // #3: Fetch all events that are cancelled
-        var cancelledEvents = await context.Events
-            .Where(e => e.Status == (int) EventStatus.Cancelled)
+
+        var cancelled = await context.Events
+            .Where(e => e.Status == EventStatus.Cancelled)
             .Select(e => new EventEditingOverview.Event(
-                e.Id,
+                e.Id.Value.ToString(),
                 e.Title
             )).ToListAsync();
-        
-        // #4: Return the answer
-        return new EventEditingOverview.Answer(eventDrafts, readyEvents, cancelledEvents);
+
+        return new EventEditingOverview.Answer(drafts, ready, cancelled);
     }
 }
--- src/Infrastructure/EfcQueries/Queries/ViewSingleEventQueryHandler.cs
+++ src/Infrastructure/EfcQueries/Queries/ViewSingleEventQueryHandler.cs
@@ -3,48 +3,39 @@ using VIAEventAssociation.Core.Domain.Aggregates.Event.Values;
 using VIAEventAssociation.Core.QueryContracts.Contract;
 using VIAEventAssociation.Core.QueryContracts.Queries;
 using VIAEventAssociation.Infrastructure.EfcDmPersistence;
-using VIAEventAssociation.Infrastructure.EfcQueries.Scaffold;
 
 namespace VIAEventAssociation.Infrastructure.EfcQueries.Queries;
 
-public class ViewSingleEventQueryHandler(VeadatabaseContext context) : IQueryHandler<ViewSingleEvent.Query, ViewSingleEvent.Answer>
+public class ViewSingleEventQueryHandler(EfcDbContext context) : IQueryHandler<ViewSingleEvent.Query, ViewSingleEvent.Answer>
 {
     public async Task<ViewSingleEvent.Answer> HandleAsync(ViewSingleEvent.Query query)
     {
-        // #1: Validate the query
-        var correctIdResult = EventId.FromString(query.EventId);
-        
-        // ? If the id is invalid, throw an exception
-        if (correctIdResult.IsFailure)
+        var eventIdResult = EventId.FromString(query.EventId);
+        if (eventIdResult.IsFailure)
         {
             throw new Exception("Invalid event id format");
         }
-
-        // #1a: Extract the id
-        var eventId = correctIdResult.Value.Value.ToString();
         
-        // #2: Query the database
         var result =  await context.Events
-            .Where(e => e.Id == eventId)
+            .Where(e => e.Id.Value == eventIdResult.Value)
             .Include(e => e.Participants)
             .Select(e => new
             {
                 EventInfo = new ViewSingleEvent.EventInfo(
-                    e.Id,
+                    e.Id.Value.ToString(),
                     e.Title,
                     e.Description,
                     "Location",
-                    e.DurationStart!,                    
+                    e.Duration!.Start.Date.ToString("dd-MM-yyyy"),
                     e.Visibility.ToString(),
                     e.Participants.Count,
                     e.Capacity
                 ),
                 Guests = e.Participants.Select(p =>
-                        new ViewSingleEvent.Guest(p.Avatar != null ? p.Avatar.ToString() : "", $"{p.FirstName} {p.LastName}"))
+                        new ViewSingleEvent.Guest(p.Avatar != null ? p.Avatar.ToString() : "", p.FullName.ToString()))
                     .ToList(),
             }).SingleAsync();
         
-        // #3: Return the result
         return new ViewSingleEvent.Answer(result.EventInfo, result.Guests);
     }
 }
